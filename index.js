8bbf27fd98cfb2913068328b01be1a10e94abe94
ach <82474261+ach-git@users.noreply.github.com>
2022-03-12 00:55:30 +0100

    Cleanest

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 const config = require("./config.json");
 const Discord = require("discord.js");
 const gamedig = require("gamedig");
-
 const client = new Discord.Client();
 
 client.config = config;
-
-var nb = 1000
-
-const updateActivity = async () => {		
-	const stats = await gamedig.query({
-        type: "garrysmod",
-	    host: config.playerCountServerIP,
-	    port: config.playerCountServerPort
-	});
-		
-	if (stats.raw.numplayers != nb) {
-		client.user.setActivity(`Joueur(s) : ${stats.raw.numplayers}`, { type: 'PLAYING' });
-    }
-};
 
 client.on("ready", () => {
 
 	console.log("[BOT] Connected to Discord.");
 	
-	updateActivity();
-	setInterval(updateActivity, 60000*0.05);
-	
+	setInterval(() => {
+            gamedig.query({
+		type: "garrysmod",
+	    	host: config.playerCountServerIP,
+	    	port: config.playerCountServerPort
+	}).then((state) => {							 
+		client.user.setActivity(`Joueur(s): ${state.players.length}/${state.maxplayers}`, {type: "PLAYING"});
+	}).catch((error) => {
+		client.user.setActivity(`:x: Serveur Hors-Ligne`, {type: "PLAYING"});
+        });
+      }, 15000); 	
 });
 
 client.login(config.token);
